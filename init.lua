515762c2155949a51088d347cff3023b3cdaf600
jimrothstein <jimrothstein@gmail.com>
2023-06-14 18:53:54 -0700

    14JUNE ... trying to set up neovim with LSP zero + lazy.nvim,  but NOT the complete startup set

--- init.lua
+++ init.lua
@@ -17,15 +17,81 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct 
 
--- get plugins ready
+-- plugins to include
 require('lazy').setup({
   ---
   -- List of plugins...
-  {'folke/tokyonight.nvim'},
+  { "ellisonleao/gruvbox.nvim" }, -- I prefer
+  -- {'folke/tokyonight.nvim'},
   {'jalvesaq/Nvim-R'},
+  {'nvim-lua/plenary.nvim'},  -- telescope needs
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = { spelling = true },
+    defaults = {
+      mode = { "n", "v" },
+      ["g"] = { name = "+goto" },
+      ["gz"] = { name = "+surround" },
+      ["]"] = { name = "+next" },
+      ["["] = { name = "+prev" },
+      ["<leader><tab>"] = { name = "+tabs" },
+      ["<leader>b"] = { name = "+buffer" },
+      ["<leader>c"] = { name = "+code" },
+      ["<leader>f"] = { name = "+file/find" },
+      ["<leader>g"] = { name = "+git" },
+      ["<leader>gh"] = { name = "+hunks" },
+      ["<leader>q"] = { name = "+quit/session" },
+      ["<leader>s"] = { name = "+search" },
+      ["<leader>u"] = { name = "+ui" },
+      ["<leader>w"] = { name = "+windows" },
+      ["<leader>x"] = { name = "+diagnostics/quickfix" },
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
+},
 
-  -- lsp-zero
+{"folke/neodev.nvim",},
+
+ -- change some telescope options and a keymap to browse plugin files
+--   {
+--     "nvim-telescope/telescope.nvim",
+--     keys = {
+--       -- add a keymap to browse plugin files
+--       -- stylua: ignore
+--       {
+--         "<leader>fp",
+--         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+--         desc = "Find Plugin File",
+--       },
+--     },
+--     -- change some options
+--     opts = {
+--       defaults = {
+--         layout_strategy = "horizontal",
+--         layout_config = { prompt_position = "top" },
+--         sorting_strategy = "ascending",
+--         winblend = 0,
+--       },
+--     },
+--   },
+---------------------
+-- init.lua:
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+-- or                              , branch = '0.1.1',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
+---------------------
+  -- lsp-zero plugin (for LSP server & related config)
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
@@ -50,7 +116,8 @@ require('lazy').setup({
 
 -- call &  load
 vim.opt.termguicolors = true
-vim.cmd.colorscheme('tokyonight')
+vim.cmd.colorscheme('gruvbox')
+--
 local lsp = require('lsp-zero').preset({})
 
 -- lsp config
@@ -62,3 +129,4 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
